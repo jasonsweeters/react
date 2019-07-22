40b8bfd2073d969a6f688f188f8ec59fb5bfb105
jasonsweeters <mail@jasonsweeters.com>
2019-07-21 20:14:22 -0400

    backing up changes

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,37 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
 
-import { 
-  Button, 
+import {
+  Button,
   Form,
   FormGroup,
   Label,
-  Input
+  Input,
+  Container,
+  Row,
+  CardColumns,
+  Card,
+  Toast,
+  Col
 } from 'reactstrap';
 
 function CreateUser() {
   return (
     <div className="App">
-   <div>
-   <Form>
-        <FormGroup>
-          <Input type="email" name="email" id="username" placeholder="Username *" required="true"/>
-          <Input type="email" name="email" id="emailAddress" placeholder="Email Address *" required="true"/>
-          <Input type="email" name="email" id="phoneNumber" placeholder="Phone Number *" required="true"/>
-          <Input type="email" name="email" id="firstName" placeholder="First Name *" required="true"/>
-          <Input type="email" name="email" id="lastName" placeholder="Last Name *" required="true"/>
-          <Input type="email" name="email" id="streetAddress" placeholder="Street Address *" required="true"/>
-          <Input type="email" name="email" id="unitNumber" placeholder="Unit Number"/>
-          <Input type="email" name="email" id="city" placeholder="City *" required="true"/>
-          <Input type="email" name="email" id="state" placeholder="State *" required="true"/>
-          <Input type="email" name="email" id="zip" placeholder="Zip *" required="true"/>
-        </FormGroup>
-        <Button>Cancel</Button>
-        <Button>Submit</Button>
-      </Form>
-   </div>
+      <div>
+        <Row>
+          <Col sm="6">
+            <Card body>
+              <Form>
+                <FormGroup>
+                  <Input name="username" id="username" placeholder="Username *" required="true" />
+                  <Input type="email" name="email" id="emailAddress" placeholder="Email Address *" required="true" />
+                  <Input name="phoneNumber" id="phoneNumber" placeholder="Phone Number *" required="true" />
+                  <Input name="firstName" id="firstName" placeholder="First Name *" required="true" />
+                  <Input name="lastName" id="lastName" placeholder="Last Name *" required="true" />
+                  <Input name="streetAddress" id="streetAddress" placeholder="Street Address *" required="true" />
+                  <Input name="unitNumber" id="unitNumber" placeholder="Unit Number" />
+                  <Input name="city" id="city" placeholder="City *" required="true" />
+                  <Input name="state" id="state" placeholder="State *" required="true" />
+                  <Input name="zip" id="zip" placeholder="Zip *" required="true" />
+                </FormGroup>
+                <Button>Cancel</Button>
+                <Button>Submit</Button>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
