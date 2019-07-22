7ae6826e3f592a9fe26a32fb59b89364a465c1b8
jasonsweeters <mail@jasonsweeters.com>
2019-07-21 21:04:38 -0400

    backing up changes

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,50 +1,82 @@
 import React from 'react';
-import './App.css';
 import './bootstrap.min.css';
 
 import {
   Button,
   Form,
   FormGroup,
-  Label,
   Input,
   Container,
   Row,
-  CardColumns,
+  Col,
   Card,
+  CardFooter,
+  CardHeader,
   Toast,
-  Col
+  ToastHeader,
+  ToastBody,
 } from 'reactstrap';
 
-function CreateUser() {
-  return (
-    <div className="App">
-      <div>
-        <Row>
-          <Col sm="6">
-            <Card body>
-              <Form>
-                <FormGroup>
-                  <Input name="username" id="username" placeholder="Username *" required="true" />
-                  <Input type="email" name="email" id="emailAddress" placeholder="Email Address *" required="true" />
-                  <Input name="phoneNumber" id="phoneNumber" placeholder="Phone Number *" required="true" />
-                  <Input name="firstName" id="firstName" placeholder="First Name *" required="true" />
-                  <Input name="lastName" id="lastName" placeholder="Last Name *" required="true" />
-                  <Input name="streetAddress" id="streetAddress" placeholder="Street Address *" required="true" />
-                  <Input name="unitNumber" id="unitNumber" placeholder="Unit Number" />
-                  <Input name="city" id="city" placeholder="City *" required="true" />
-                  <Input name="state" id="state" placeholder="State *" required="true" />
-                  <Input name="zip" id="zip" placeholder="Zip *" required="true" />
-                </FormGroup>
-                <Button>Cancel</Button>
-                <Button>Submit</Button>
-              </Form>
-            </Card>
-          </Col>
-        </Row>
+class CreateUser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showToast: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      show: !this.state.showToast
+    });
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <Container>
+          <Row>
+            <Col sm="6">
+              <Card body>
+                <CardHeader>
+                  Create User
+                </CardHeader>
+                <Form>
+                  <FormGroup>
+                    <Input name="username" id="username" placeholder="Username *" required="true" />
+                    <Input type="email" name="email" id="emailAddress" placeholder="Email Address *" required="true" />
+                    <Input name="phoneNumber" id="phoneNumber" placeholder="Phone Number *" required="true" />
+                    <Input name="firstName" id="firstName" placeholder="First Name *" required="true" />
+                    <Input name="lastName" id="lastName" placeholder="Last Name *" required="true" />
+                    <Input name="streetAddress" id="streetAddress" placeholder="Street Address *" required="true" />
+                    <Input name="unitNumber" id="unitNumber" placeholder="Unit Number" />
+                    <Input name="city" id="city" placeholder="City *" required="true" />
+                    <Input name="state" id="state" placeholder="State *" required="true" />
+                    <Input name="zip" id="zip" placeholder="Zip *" required="true" />
+                  </FormGroup>
+                </Form>
+                <CardFooter className="text-right">
+                <Toast isOpen={this.state.showToast}>
+                    <ToastHeader>
+                      Success!
+                    </ToastHeader>
+                    <ToastBody>
+                      The User was successfully created.
+                    </ToastBody>
+                  </Toast>
+                  <Button>Cancel</Button>
+                  <Button>Submit</Button>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CreateUser;
